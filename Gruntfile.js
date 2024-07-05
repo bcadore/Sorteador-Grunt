e7ca2e6c816d4b42bedc9ac7603547669eaf3a4f
bernardo <bernardo.cadore95@gmail.com>
2024-07-04 23:22:53 -0300

    Adicionado grunt-watch ao projeto.

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,10 +16,17 @@ module.exports = function (grunt) {
         },
       },
     },
+    watch: {
+      less: {
+        files: ["src/styles/**/*.less"],
+        tasks: ["less:development"],
+      },
+    },
   });
 
   grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-contrib-watch");
 
-  grunt.registerTask("default", ["less:development"]);
+  grunt.registerTask("default", ["watch"]);
   grunt.registerTask("build", ["less:production"]);
 };
